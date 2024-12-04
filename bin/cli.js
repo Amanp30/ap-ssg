ac5128b5564d42d78878ec286bb8e99d592bc9df
aman <amanpareek300@gmail.com>
2024-12-04 20:36:05 +0530

    feat: add `init` command to initialize config
    - Added `npx ap-ssg init` command to generate `apssg.config.js` file

--- bin/cli.js
+++ bin/cli.js
@@ -1,23 +1,34 @@
 #!/usr/bin/env node
 
-const { ssg } = require("../lib");
 const folderSetup = require("../lib/utils/folderSetup");
+const generateConfig = require("../lib/utils/generateConfig");
 
 switch (process.argv[2]) {
   case "build":
+    const { ssg } = require("../lib");
     ssg.generate();
     break;
 
   case "watch":
-    ssg.watch();
+    const { ssg: ssgWatch } = require("../lib");
+    ssgWatch.watch();
     break;
 
   case "folder-setup":
     folderSetup();
     break;
 
+  case "init":
+    generateConfig().then(console.log);
+    break;
+
   default:
-    console.log("Unknown command ");
-    console.log("   - use `npx ap-ssg build` to generate build  ");
+    console.log("Unknown command.");
+    console.log("   - use `npx ap-ssg build` to generate build");
+    // console.log("   - use `npx ap-ssg watch` to watch for changes");
+    console.log("   - use `npx ap-ssg folder-setup` to setup folder structure");
+    console.log(
+      "   - use `npx ap-ssg init` to initialize the config file and structure",
+    );
     break;
 }
